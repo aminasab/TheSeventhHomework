48ad2a8903069caa9a47142998e4de33e99062a9
Amina S <aminchik2809@mail.ru>
2023-03-09 01:01:39 +0300

    add files

---
+++ FibonacciCalculation.cs
@@ -0,0 +1,27 @@
+﻿namespace TheSeventhProgram
+{
+    internal class FibonacciCalculation
+    {
+        internal static int CalculationFibonacciUsingTheRecursiveFunction(int n)
+        {
+            if (n == 0 || n == 1)
+                return n;
+            return CalculationFibonacciUsingTheRecursiveFunction(n - 1) + CalculationFibonacciUsingTheRecursiveFunction(n - 2);
+        }
+
+        public static int CalculationFibonacciUsingTheCyclicMethod(int n)
+        {
+            int result = 0;
+            int b = 1;
+            int tmp;
+
+            for (int i = 0; i < n; i++)
+            {
+                tmp = result;
+                result = b;
+                b += tmp;
+            }
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace TheSeventhProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Console.WriteLine(FibonacciCalculation.CalculationFibonacciUsingTheRecursiveFunction(13));
+            stopwatch.Stop();
+            TimeSpan timeSpan1 = stopwatch.Elapsed;
+            Console.WriteLine("Время работы вычисления чисел Фибоначчи с помощью рекурсивной функции составило- " + timeSpan1);
+            stopwatch.Restart();
+            Console.WriteLine(FibonacciCalculation.CalculationFibonacciUsingTheCyclicMethod(13));
+            stopwatch.Stop();
+            TimeSpan timeSpan2 = stopwatch.Elapsed;
+            Console.WriteLine("Время работы вычисления чисел Фибоначчи с помощью циклов составило- " + timeSpan2);
+        }
+    }
+}
